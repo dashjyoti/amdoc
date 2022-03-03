34e3023bd2cd4a23955e6ad791f7b7f0e0ee861f
dashjyoti <78753161+dashjyoti@users.noreply.github.com>
2022-03-03 18:44:40 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,12 @@
+public class Loop
+
+{
+    public static void main(String args[])
+    {
+        int i=4;
+        for(i=4; i<=40; i++)
+        {System.out.println("result of=" +i);
+
+    }
+}
+}
